34c6978f6e9b2d9c2d5063f5e9511f73e16be6bb
Robin Millette <robin@millette.info>
2017-05-24 16:27:55 -0400

    Remove a and b pages.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ export default () => <div>
   <p>Hello!</p>
   <ul>
     <li><Link href='/db'><a>DB</a></Link></li>
-    <li><Link href='/b' as='/a'><a>a</a></Link></li>
-    <li><Link href='/a' as='/b'><a>b</a></Link></li>
   </ul>
+  <p>Goodbye</p>
 </div>
