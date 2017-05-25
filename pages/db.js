837a9eb2d7a23e480b01e281701516f456693b65
Robin Millette <robin@millette.info>
2017-05-25 05:39:23 -0400

    Tiny refactor.

--- pages/db.js
+++ pages/db.js
@@ -4,14 +4,19 @@ import fetch from 'isomorphic-fetch'
 import pick from 'lodash.pick'
 import Header from '../components/progress'
 
+const dbUrl = (yo) => {
+  const host = (yo.req && yo.req.headers && yo.req.headers.host) ||
+    (window !== 'undefined' && window.location.host)
+  if (!host) { throw new Error('Weird host thing.') }
+  const u = [host.indexOf('.') === -1 ? 'http:/' : 'https:/', host, 'api']
+  if (yo.query.what) { u.push(yo.query.what) }
+  return u.join('/')
+}
+
 export default class MyDBPage extends React.Component {
   static async getInitialProps (yo) {
-    const host = (yo.req && yo.req.headers && yo.req.headers.host) ||
-      (window !== 'undefined' && window.location.host)
-    if (!host) { throw new Error('Weird host thing.') }
-    const u = [host.indexOf('.') === -1 ? 'http:/' : 'https:/', host, 'api']
-    if (yo.query.what) { u.push(yo.query.what) }
-    const res = await fetch(u.join('/'))
+    const u = dbUrl(yo)
+    const res = await fetch(u)
     return res.ok ? res.json() : pick(res, 'status', 'statusText')
   }
 
