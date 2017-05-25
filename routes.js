1395668dec7625dd789abce40b69c6fdaf749990
Robin Millette <robin@millette.info>
2017-05-25 07:20:55 -0400

    Shorter lines.

--- routes.js
+++ routes.js
@@ -4,13 +4,15 @@
 require('dotenv-safe').load()
 const fetch = require('isomorphic-fetch')
 
-const readAuth = 'Basic ' + Buffer.from([process.env.ReadKey, process.env.ReadPassword].join(':')).toString('base64')
+const readAuth = Buffer
+  .from([process.env.ReadKey, process.env.ReadPassword].join(':'))
+  .toString('base64')
 
 module.exports = (router, app) => {
   const handle = app.getRequestHandler()
 
   router.get('/api/:more*', async (ctx) => {
-    const headers = { authorization: readAuth }
+    const headers = { authorization: 'Basic ' + readAuth }
     const u = [process.env.DBServer, process.env.DBName]
     if (ctx.params.more) { u.push(ctx.params.more) }
     const res = await fetch(u.join('/'), { headers })
@@ -19,7 +21,8 @@ module.exports = (router, app) => {
   })
 
   router.get('/db/:more+', async (ctx) => {
-    await app.render(ctx.req, ctx.res, '/db', Object.assign({}, ctx.query, { what: ctx.params.more }))
+    const obj = Object.assign({}, ctx.query, { what: ctx.params.more })
+    await app.render(ctx.req, ctx.res, '/db', obj)
     ctx.respond = false
   })
 
