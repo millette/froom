e4d7fce9c347c3adceb17643812d9a09a4db633b
Robin Millette <robin@millette.info>
2017-05-27 13:19:59 -0400

    Route /aye/:id to show each doc.

---
+++ components/docview.js
@@ -0,0 +1,21 @@
+// npm
+import React from 'react'
+import Link from 'next/link'
+
+export default ({ single, id, le1, le2, le3, le4 }) => <div className='callout'>
+    {single && <h2>
+      {le1}
+      {id && <small> {id}</small>}
+    </h2>}
+    {!single && <h2>
+      <Link as={'/aye/' + id} href={{ pathname: '/aye', query: { id } }}>
+        <a>
+          {le1}
+          {id && <small> {id}</small>}
+        </a>
+      </Link>
+    </h2>}
+  <h3>{le2}</h3>
+  <p>{le3}</p>
+  <p>{le4}</p>
+</div>
--- pages/aye.js
+++ pages/aye.js
@@ -6,11 +6,16 @@ import pick from 'lodash.pick'
 
 // self
 import Header from '../components/progress'
-import MyFormViewComp from '../components/formview'
+import DocViewComp from '../components/docview'
 import utils from '../utils'
 
 export default class MyAyePage extends React.Component {
   static async getInitialProps (yo) {
+    if (yo.query.id) {
+      const u = utils.dbUrl(yo)
+      const res = await fetch([u, yo.query.id].join('/'))
+      return res.ok ? res.json() : pick(res, 'status', 'statusText')
+    }
     yo.query.what = '_all_docs'
     const u = utils.dbUrl(yo)
     const res = await fetch(u + '?include_docs=true&limit=5')
@@ -18,6 +23,16 @@ export default class MyAyePage extends React.Component {
   }
 
   render () {
+    const a = (
+      this.props.rows && this.props.rows.map((row) => <DocViewComp key={row.id} le1={row.doc.le1} le2={row.doc.le2} le3={row.doc.le3} le4={row.doc.le4} id={row.id} />)
+    )
+
+    const b = (
+      <DocViewComp single le1={this.props.le1} le2={this.props.le2} le3={this.props.le3} le4={this.props.le4} id={this.props._id} />
+    )
+
+    const c = this.props.rows ? a : b
+
     return <div>
       <Header>
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css' integrity='sha256-itWEYdFWzZPBG78bJOOiQIn06QCgN/F0wMDcC4nOhxY=' crossorigin='anonymous' />
@@ -26,8 +41,9 @@ export default class MyAyePage extends React.Component {
         <ul className='menu'>
           <li><Link href='/'><a>TOP</a></Link></li>
           <li><Link href='/db'><a>DB</a></Link></li>
+          <li><Link href='/aye'><a>AYE</a></Link></li>
         </ul>
-        {this.props.rows.map((row) => <MyFormViewComp key={row.id} le1={row.doc.le1} le2={row.doc.le2} le3={row.doc.le3} le4={row.doc.le4} id={row.id} />)}
+        {c}
       </div>
     </div>
   }
